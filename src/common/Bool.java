983fbd7cb0d6fc0ec3d1d842e585c031831d00ef
80ROkWOC4j <80ROkWOC4j@gmail.com>
2020-06-06 04:14:00 +0900

    이클립스와 호환성 점검

--- src/common/Bool.java
+++ src/common/Bool.java
@@ -1,4 +1,27 @@
 package common;
 
 public enum Bool {
+    TRUE(true, "Y"),
+    FALSE(false, "N");
+
+    public final boolean bool;
+    public final String yn;
+
+    Bool(boolean bool, String yn){
+        this.bool = bool;
+        this.yn = yn;
+    }
+
+    public static Bool get(boolean obj){
+        if(Bool.TRUE.bool == obj)
+            return Bool.TRUE;
+        else
+            return Bool.FALSE;
+    }
+    public static Bool get(String obj){
+        if(Bool.TRUE.yn.equals(obj))
+            return Bool.TRUE;
+        else
+            return Bool.FALSE;
+    }
 }
