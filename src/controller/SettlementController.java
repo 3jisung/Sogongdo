87890fe3799a4415ce725fe4b883e2fb09532c20
sm <80ROkWOC4j@gmail.com>
2020-06-25 18:02:00 +0900

    minor update

--- src/controller/SettlementController.java
+++ src/controller/SettlementController.java
@@ -1,16 +1,30 @@
 package controller;
 
+import common.SettlementDAO;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
 
 @WebServlet("/settlement")
 public class SettlementController extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.service(req, resp);
+        req.setAttribute("reservationList", SettlementDAO.INSTANCE.selectSettlements(null, null, null));
+        ServletContext context = req.getServletContext();
+
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/settlement.jsp");
+        dispatcher.forward(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
     }
 }
