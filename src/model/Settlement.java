9f5c469d3d6946759158f550919e495284b87073
sm <80ROkWOC4j@gmail.com>
2020-06-26 05:05:54 +0900

    settlement 중간 커밋

--- src/model/Settlement.java
+++ src/model/Settlement.java
@@ -5,15 +5,15 @@ import java.sql.Timestamp;
 
 public class Settlement {
     public final Date settlement_date;
-    public final String work_place;
-    public final String admin;
-    public final Timestamp processing_date;
+    public final String workplace;
+    public final String admin_id;
+    public final Timestamp processed_time;
     // processing_time aka settlement_time
 
-    public Settlement(Date settlement_date, String work_place, String admin, Timestamp processing_date) {
+    public Settlement(Date settlement_date, String workplace, String admin_id, Timestamp processed_time) {
         this.settlement_date = settlement_date;
-        this.work_place = work_place;
-        this.admin = admin;
-        this.processing_date = processing_date;
+        this.workplace = workplace;
+        this.admin_id = admin_id;
+        this.processed_time = processed_time;
     }
 }
