35ae0d58c8f064fb2161a6313a91eaa54a4c8658
예승 차 <cys7818@naver.com>
2020-06-26 04:33:03 +0900

    authority 구현중

--- src/model/Authority.java
+++ src/model/Authority.java
@@ -1,29 +1,21 @@
 package model;
 
 public class Authority {
-	private int id;
 	private String target;
 	private String action;
 	
-	public Authority(int id, String target, String action) {
-		this.id =id;
+	public Authority(String target, String action) {
 		this.target = target;
 		this.action = action;
 	}
-	
-	public void setId(int id) {
-		this.id = id;
-	}
+
 	public void setTarget(String target) {
 		this.target = target;
 	}
 	public void setAction(String action) {
 		this.action = action;
 	}
-	
-	public int getId() {
-		return id;
-	}
+
 	public String getTarget() {
 		return target;
 	}
